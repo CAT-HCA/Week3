173d06b9f1d72ec02b4b0f0fb018febf3d37adf4
Cori <corinne.a.ader@gmail.com>
2019-08-07 09:52:50 -0400

    formatting images with js lab

---
+++ Mod2/MyWork/pg2-22ex1/scripts/javascript_css.js
@@ -0,0 +1,9 @@
+"use strict";
+
+window.onload = function ()
+{
+    let imageFiles = ["images/cat1.jpeg", "images/cat2.jpeg", "images/cat3.jpeg" ];
+    let imageDescriptions = ["First Cat", "Second Cat", "Third Cat"];
+    
+}
+

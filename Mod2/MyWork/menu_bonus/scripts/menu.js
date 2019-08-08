ad3260e086f264f9f9eff7b8ffce8a7cfa0592b7
Cori <corinne.a.ader@gmail.com>
2019-08-08 11:22:49 -0400

    created menu array

---
+++ Mod2/MyWork/menu_bonus/scripts/menu.js
@@ -0,0 +1,40 @@
+"use strict";
+
+[
+    {
+        Meal:"Breakfast",
+        Menu: [
+            {item:"Blueberry Pancakes", price: 5.25},
+            {item:"Banana Pancakes", price: 5.25},
+            {item:"Chocolate Chip Pancakes", price: 5.25},
+            {item:"French toast", price: 5.00}
+        ]
+    },
+    {
+        Meal:"Lunch",
+        Menu: [
+            {item:"Filet Salmon", price: 8.25},
+            {item:"Meatloaf", price: 7.95},
+            {item:"Chicken Scampi", price: 9.25},
+            {item:"Breaded Chicken Parmigiana", price: 7.25}
+        ]
+    },
+    {
+        Meal:"Dinner",
+        Menu: [
+            {item:"Gyro Wrap", price: 6.99},
+            {item:"Fish & Chips Wrap", price: 7.15},
+            {item:"Chicken Florentine ", price: 7.15},
+            {item:"Chef's Salad", price: 7.25}
+        ]
+    },
+    {
+        Meal:"Dessert",
+        Menu: [
+            {item:"Cheese Cake", price: 3.25},
+            {item:"Ice Cream", price: 1.55},
+            {item:"Jello", price: 2.25},
+            {item:"Pudding", price: 2.35}
+        ]
+    }
+]
